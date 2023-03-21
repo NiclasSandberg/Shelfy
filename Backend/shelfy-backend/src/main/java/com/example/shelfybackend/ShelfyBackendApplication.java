d265e613cdaa5d37accf3fa53f6c7cfad232ef69
Céline <64008463+clnksr@users.noreply.github.com>
2023-03-21 15:12:26 +0100

    added backend folder

---
+++ Backend/shelfy-backend/src/main/java/com/example/shelfybackend/ShelfyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shelfybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelfyBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/shelfy-backend/src/test/java/com/example/shelfybackend/ShelfyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shelfybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
