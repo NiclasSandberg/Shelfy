0aac6877785bbd638cbaee744f8bb0859b387bf3
NiclasSandberg <nike_pike89@hotmail.com>
2023-03-24 15:49:10 +0100

    working on expiry date calculations

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -9,6 +9,41 @@ interface ProductCardAttrs {
 
 const ProductCard = ({ product }: ProductCardAttrs) => {
 
+
+  function datediff(date:Date) {
+    let d1 = date;
+    let d2 = now = new Date();
+    if (d2.getTime() < d1.getTime()) {
+      d1 = d2;
+      d2 = date;
+    }
+    let yd = d1.getFullYear();
+    let yn = d2.getFullYear();
+    let years = yn - yd;
+    let md = d1.getMonth();
+    let mn = d2.getMonth();
+    let months = mn - md;
+    if (months < 0) {
+      years--;
+      months = 12 - md + mn;
+    }
+    let dd = d1.getDate();
+    let dn = d2.getDate();
+    let days = dn - dd;
+    if (days < 0) {
+      months--;
+      // figure out how many days there are in the last month
+      d2.setMonth(mn, 0);
+      days = d2.getDate() - dd + dn;
+    }
+    let weeks = Math.floor(days / 7);
+    days = days % 7;
+    if (years > 0) return years + ' years' + (months > 0 ? ' and ' + months + ' months' : '');
+    if (months > 0) return months + ' months' + (weeks > 0 ? ' and ' + weeks + ' weeks' : '');
+    if (weeks > 0) return weeks + ' weeks' + (days > 0 ? ' and ' + days + ' days' : '');
+    return days + ' days';
+ }
+
   const today = new Date();
   const expiryDate = new Date(product.expiryDate);
   const productOpenedAt = new Date(product.dateOpened);
@@ -16,21 +51,19 @@ const ProductCard = ({ product }: ProductCardAttrs) => {
   const shelflifeLeftFromTodayInMs = expiryDate.getTime() - today.getTime();
   const fullShelflifeInMs = expiryDate.getTime() - productOpenedAt.getTime();
   console.log(fullShelflifeInMs);
-  const progressShelflifeInMs = fullShelflifeInMs - shelflifeLeftFromTodayInMs;
   const percentageToShow = Math.ceil(100 - (shelflifeLeftFromTodayInMs / (fullShelflifeInMs / 100)));
   console.log("percentage to show: "+percentageToShow)
 
-  let daysUntilExpire = Math.ceil(shelflifeLeftFromTodayInMs / (1000 * 60 * 60 * 24));
-  let months = Math.ceil(daysUntilExpire / 31);
-  let daysAfterMonthsSubstraction = daysUntilExpire - (months * 30);
+  let daysUntilExpireFromToday = Math.floor(shelflifeLeftFromTodayInMs / (1000 * 60 * 60 * 24));
+  let months = Math.floor(daysUntilExpireFromToday / 30);
+  let daysAfterMonthsSubstraction = daysUntilExpireFromToday % months;
+  console.log("remainder days (modulo)" + daysAfterMonthsSubstraction)
 
   console.log("difference in days: " + Math.ceil(shelflifeLeftFromTodayInMs / (1000 * 60 * 60 * 24)))
   console.log("months left: " + months);
 
-  // 0 = day opened == today // 100% shelflife (date of expiry - date opened )
-// 100 = 0 days left
-// 12M = 365    2022-12-31(opened) ? 2023-07-21 (expiry date)
-//  
+
+
   return (
     <>
       <Link to={'/products/' + product.id} >
@@ -38,13 +71,16 @@ const ProductCard = ({ product }: ProductCardAttrs) => {
           {product.name} <br />
           {product.dateOpened} <br />
           {product.expiryDate}<br />
+ 
+          {datediff(product.expiryDate)}
+          
+{/* 
+          {daysUntilExpireFromToday < 30 ?
+          "it's still good for " + daysUntilExpireFromToday + " days" :
+          "its still good for " + months + " months and " + daysAfterMonthsSubstraction + " days"} */}
 
-          {daysUntilExpire < 30 ?
-          "it's still good for " + daysUntilExpire + " days" :
-          "its still good for " + months + " months and " + daysAfterMonthsSubstraction + " days"}
-
-          <CircularProgress key={product.name} size="lg" determinate value={percentageToShow > 95 ? 92 : percentageToShow }>
-            {daysUntilExpire < 30 ? daysUntilExpire  + " D" : months + " M"}
+          <CircularProgress key={product.name} size="lg" determinate value={percentageToShow > 95 && percentageToShow < 100 ? 92 : percentageToShow }>
+            {daysUntilExpireFromToday < 30 ? daysUntilExpireFromToday  + " D" : months + " M"}
           </CircularProgress>
           <br />
           <br />
