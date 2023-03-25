7ec668461b41a566f6e739363c9feed2129e8fc3
NiclasSandberg <nike_pike89@hotmail.com>
2023-03-25 14:24:56 +0100

    app.js now identical

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,60 +5,21 @@ import ProductView from './components/ProductView';
 import CreateProduct from './components/CreateProduct';
 
 function App() {
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [months, setMonths] = useState("");
-  
-  useEffect(() => {
-    getData();
-  }, [months]) 
+  <>
+      <BrowserRouter>
 
-  const getData = async () => {
-    const products = await getProducts();
-    console.log({products});
-    setProducts(products);
-  }
-  
-  const handleChange = (event: SelectChangeEvent) => {
-    setMonths(event.target.value);
-  };
+        <Routes>
+          <Route path="/products/new" element={<CreateProduct />} /> 
+          <Route path="/products/:productId" element={<ProductView />} />
+          <Route path='/' element={<ProductList />} />
+          {/* <Route path="/articles/:articleId/edit" element={<ArticleEdit />} /> */}
 
-  
-  return (
-    <>
-    <h1>Shelfy</h1> 
-    {/*products.length === 0 && "Loading ..."*/}
-    {products?.map((prod,index) => 
-    <div>
-       <ProductCard product={prod} key={prod.id} /> 
-       <CircularProgress key={prod.name} size="lg" determinate value={66.67}>
-       {prod.daysUntilExpiry}
-     </CircularProgress>
-     </div>
-       )}
-       <div style={{paddingTop:"1rem"}}>
-        <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Months left</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={months}
-          label="Months left"
-          onChange={handleChange}
-        >
-         
-          <MenuItem value={1}>1M</MenuItem>
-          <MenuItem value={6}>6M</MenuItem>
-          <MenuItem value={12}>12M</MenuItem>
-        </Select>
-      </FormControl>
-     {/*<LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker />
-    </LocalizationProvider>*/}
-    {months}M
-    </div>
+        </Routes>
+      </BrowserRouter>
+
+
     </>
 
-  );
 }
 
 export default App;
