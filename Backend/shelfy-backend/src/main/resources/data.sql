12d3e58a907211d7f60b5f1df31ca206a0c1ee84
NiclasSandberg <nike_pike89@hotmail.com>
2023-03-29 17:18:27 +0200

    mock data added

--- Backend/shelfy-backend/src/main/resources/data.sql
+++ Backend/shelfy-backend/src/main/resources/data.sql
@@ -1,12 +1,16 @@
 INSERT INTO category(id, name, image_url) VALUES (1, 'Makeup', '/category-images/makeup.png');
 INSERT INTO category(id, name, image_url) VALUES (2, 'Skincare', '/category-images/skincare.png');
-INSERT INTO category(id, name, image_url) VALUES (3, 'Hair care', '/category-images/hair-care.png');
+INSERT INTO category(id, name, image_url) VALUES (3, 'Hair care', '/category-images/haircare.png');
 INSERT INTO category(id, name, image_url) VALUES (4, 'Medicine', '/category-images/medicine.png');
 INSERT INTO category(id, name, image_url) VALUES (5, 'Towels', '/category-images/towels.png');
 INSERT INTO category(id, name, image_url) VALUES (6, 'Miscellaneous', '/category-images/miscellaneous.png');
 
 
-INSERT INTO product(id, name, description, date_opened, expiry_date, period_after_opening, category_id) VALUES (123456, 'Toothbrush', 'This is my toothbrush','2023-03-21', '2024-03-21', 6, 6);
-INSERT INTO product(id, name, description, date_opened, expiry_date, period_after_opening, category_id) VALUES (12345, 'Sunscreen', 'Use daily','2022-12-31', '2023-07-21', 2, 2);
-INSERT INTO product(id, name, description, date_opened, expiry_date, period_after_opening, category_id) VALUES (1234, 'Mascara', 'This is my mascara','2023-01-01', '2023-06-14', 1, 1);
-INSERT INTO product(id, name, description, date_opened, expiry_date, period_after_opening, category_id) VALUES (123, 'Brush', 'This is my brush','2022-01-01', '2023-03-26', 3, 3);
+
+INSERT INTO product(id, name, description, date_opened, expiry_date, period_after_opening, category_id) VALUES (12345, 'Sunscreen', 'Use daily','2023-03-29', '2024-03-29', 12, 2);
+INSERT INTO product(id, name, description, date_opened, expiry_date, period_after_opening, category_id) VALUES (1234, 'Mascara', 'From the brand Benefit, black, lengthening.','2023-01-01', '2023-06-01', 6, 1);
+INSERT INTO product(id, name, description, date_opened, expiry_date, period_after_opening, category_id) VALUES (1239, 'Leave-in Conditioner', 'Heat protection product from this famous brand.','2023-01-01', '2023-09-01', 9, 3);
+INSERT INTO product(id, name, description, date_opened, expiry_date, period_after_opening, category_id) VALUES (1238, 'Toothbrush', 'Bamboo toothbrush, soft bristles.','2023-02-01', '2023-05-01', 3, 6);
+INSERT INTO product(id, name, description, date_opened, expiry_date, period_after_opening, category_id) VALUES (1232, 'Shower towel', 'Big one in the bathroom downstairs.','2023-03-29', '2023-04-05', 1, 5);
+INSERT INTO product(id, name, description, date_opened, expiry_date, period_after_opening, category_id) VALUES (123456, 'Cough syrup', 'In the medicine cabinet behind the towels.','2022-03-21', '2024-03-21', 24, 4);
+INSERT INTO product(id, name, description, date_opened, expiry_date, period_after_opening, category_id) VALUES (123, 'Shower towel 2', 'Big one in the bathroom upstairs.','2023-03-29', '2023-04-05', 1, 5);
